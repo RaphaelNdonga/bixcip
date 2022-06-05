4e74a91bb5b88bace85a1532656770a2ed36afc0
RaphaelNdonga <nkibi53@gmail.com>
2022-06-05 14:05:32 +0300

    changes

--- lottery-dapp/blockchain/interfaces/IRandomNumberGenerator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.7;
-
-interface IRandomNumberGenerator {
-    function requestRandomWords() external;
-
-    function getRandomWords() external view returns (uint256[] memory);
-}
