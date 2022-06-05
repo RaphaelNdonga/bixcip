9c36972d87fa01dd72df611fdef92f7b0135044b
RaphaelNdonga <nkibi53@gmail.com>
2022-06-05 14:05:17 +0300

    mocha timeout added and link token address also added

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,11 @@ module.exports = {
       url: process.env.ALCHEMY_API_KEY,
       accounts: [process.env.PRIVATE_KEY],
       vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-      keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
+      keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+      linkToken: "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
     }
+  },
+  mocha: {
+    timeout: 200000
   }
 };
