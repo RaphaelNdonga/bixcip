789df9a52d80e0bf945e0b2cad848717fbe3a4f3
RaphaelNdonga <nkibi53@gmail.com>
2022-06-08 12:45:51 +0300

    these logs were essential in the understanding of the bixcip build folder generated by truffle

--- migrations/3_lottery_migration.js
+++ migrations/3_lottery_migration.js
@@ -2,5 +2,9 @@ const BIXCIPLottery = artifacts.require("BIXCIPLottery");
 const RandomNumberGenerator = artifacts.require("RandomNumberGenerator");
 
 module.exports = function (deployer) {
-  deployer.deploy(BIXCIPLottery, RandomNumberGenerator.address);
+  deployer.deploy(BIXCIPLottery, RandomNumberGenerator.address).then(() => {
+    console.log("Bixciplottery address: ", BIXCIPLottery.address);
+    console.log("Bixciplottery network: ", BIXCIPLottery.networks);
+    console.log("Bixciplottery abi: ", BIXCIPLottery.abi);
+  });
 };
