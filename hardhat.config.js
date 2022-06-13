0453991c589451948755670f397cf09ba4a82a1b
RaphaelNdonga <nkibi53@gmail.com>
2022-06-13 18:32:48 +0300

    changed the urls

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,14 +17,14 @@ module.exports = {
   },
   networks: {
     rinkeby: {
-      url: process.env.ALCHEMY_RINKEBY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_KEY}`,
+      accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY2],
       vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
       keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
       linkToken: "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
     },
     mumbai: {
-      url: process.env.ALCHEMY_MUMBAI_KEY,
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_KEY}`,
       accounts: [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY2],
       vrfCoordinator: "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed",
       keyHash: "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
