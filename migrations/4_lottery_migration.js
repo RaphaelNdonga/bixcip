458504c51060445b7c81a80d80e120e2ce7e3f26
RaphaelNdonga <nkibi53@gmail.com>
2022-06-30 15:53:02 +0300

    migrations changed to copy contract data to frontend in a better way

--- migrations/4_lottery_migration.js
+++ migrations/4_lottery_migration.js
@@ -3,17 +3,12 @@ const RandomNumberGenerator = artifacts.require("RandomNumberGenerator");
 const BIIX = artifacts.require("BIIX");
 const fs = require("fs");
 
-module.exports = function (deployer) {
-  deployer.deploy(BIXCIPLottery, RandomNumberGenerator.address, BIIX.address).then(() => {
-    console.log("Bixciplottery address: ", BIXCIPLottery.address);
-    console.log("Bixciplottery network: ", BIXCIPLottery.networks);
-    console.log("Bixciplottery abi: ", BIXCIPLottery.abi);
-    fs.copyFile(__dirname + "/../build/contracts/BIXCIPLottery.json", __dirname + "/../lottery-dapp/pages/blockchain/BIXCIPLottery.json", (err) => {
-      if (err) {
-        console.log("Error occurred while copying Lottery file")
-        throw err
-      }
-      console.log("Lottery File copied successfully");
-    })
-  });
+module.exports = async function (deployer) {
+  await deployer.deploy(BIXCIPLottery, RandomNumberGenerator.address, BIIX.address);
+  console.log("Bixciplottery address: ", BIXCIPLottery.address);
+  console.log("Bixciplottery network: ", BIXCIPLottery.networks);
+  console.log("Bixciplottery abi: ", BIXCIPLottery.abi);
+  fs.writeFileSync(__dirname + "/../lottery-dapp/pages/blockchain/BIXCIPLotteryAbi.json", JSON.stringify(BIXCIPLottery.abi));
+  fs.writeFileSync(__dirname + "/../lottery-dapp/pages/blockchain/BIXCIPLotteryAddress.json", JSON.stringify(BIXCIPLottery.address));
+
 };
